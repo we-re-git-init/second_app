6fbcad1fc691b3bce3897b85da9421f146b2672d
Brian <riceb53@gmail.com>
2019-06-23 11:45:07 -0500

    add instance variable to pass to view

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,5 +1,8 @@
 class Api::ExamplePagesController < ApplicationController
   def first_action
+    # place for ruby logic
+    @the_time = Time.now.strftime("%Y")
+
     render 'first.json.jb'
   end
 end
