4cdc51b0109e95de3aa0f20a832182cf74051968
Brian <riceb53@gmail.com>
2019-06-23 11:40:24 -0500

    render using a file rather than render json:

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,5 +1,5 @@
 class Api::ExamplePagesController < ApplicationController
   def first_action
-    render json: {message: 'hello', time: Time.now.strftime("%m/%d/%Y")}
+    render 'first.json.jb'
   end
 end
