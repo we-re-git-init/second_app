cf32fa9e4572d963ba2149a7415df397eb847e2a
Brian <riceb53@gmail.com>
2019-06-23 11:37:51 -0500

    add human readable time

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,5 +1,5 @@
 class Api::ExamplePagesController < ApplicationController
   def first_action
-    render json: {message: 'hello'}
+    render json: {message: 'hello', time: Time.now.strftime("%m/%d/%Y")}
   end
 end
